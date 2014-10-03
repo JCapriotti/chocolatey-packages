aa8b03faf9d2880bda667447ac54e614ff994707
Jason Capriotti <jason.capriotti@gmail.com>
2014-10-03 16:28:12 -0500

    Initial commit

---
+++ ConsoleZ.Settings/README.md
@@ -0,0 +1,14 @@
+ConsoleZ.Settings
+=================
+
+Allows ConsoleZ settings to be installed from a provided URL.
+
+
+Usage Example
+----- 
+Since the file you are downloading will change over time, be sure to use the -force switch.
+
+```
+choco install ConsoleZ.Settings -params 'url=https://yoursite.com/console.xml' -force
+```
+
---
+++ ConsoleZ.Settings/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿
+$packageName = "ConsoleZ.Settings"
+
+try 
+{
+	$params = $env:chocolateyPackageParameters -split ';' | ConvertFrom-StringData
+	$url = $params.url;
+	
+	if ($url -ne $null)
+	{
+		Write-Host "Downloading from URL $url"
+		Get-ChocolateyWebFile $packageName "$env:appdata\console\console.xml" $url
+	}
+	else 
+	{
+		Write-Warning "No URL specified. Try calling choco install ConsoleZ.Settings -params 'url=http://example.com'"
+	}
+	
+	Write-ChocolateySuccess "$packageName"
+} 
+catch 
+{
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+	throw
+}

6cb5015e8401a54a3f01085e2c9d405a18bfa460
Jason Capriotti <jason.capriotti@gmail.com>
2014-10-05 17:31:43 -0500

    Notepad++ settings package

---
+++ Notepadplusplus.Settings/tools/chocolateyInstall.ps1
@@ -0,0 +1,36 @@
+﻿
+$packageName = "Notepadplusplus.Settings"
+
+try 
+{
+	$params = $env:chocolateyPackageParameters -split ';' | ConvertFrom-StringData
+	$url = $params.url;
+	
+	if ($url -ne $null)
+	{
+		Write-Host "Downloading from URL $url"
+		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\config.xml" "$url/config.xml"
+		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\contextMenu.xml" "$url/contextMenu.xml"
+		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\functionList.xml" "$url/functionList.xml"
+		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\langs.xml" "$url/langs.xml"
+		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\shortcuts.xml" "$url/shortcuts.xml"
+		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\stylers.xml" "$url/stylers.xml"
+		
+		# Find the theme file
+		$themeNode = Select-Xml -Path "$env:appdata\notepad++\config.xml" -XPath "//GUIConfig[@name='stylerTheme']"
+		$themePath = $themeNode.node.path
+		$themeFile = Split-Path $themePath -Leaf
+		Get-ChocolateyWebFile $packageName " ${env:ProgramFiles(x86)}\Notepad++\themes\$themeFile" "$url/$themeFile"
+	}
+	else 
+	{
+		Write-Warning "No URL specified. Try calling choco install Notepadplusplus.Settings -params 'url=http://example.com/NotepadSettings/'"
+	}
+	
+	Write-ChocolateySuccess "$packageName"
+} 
+catch 
+{
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+	throw
+}

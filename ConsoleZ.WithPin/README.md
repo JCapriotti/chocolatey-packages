48297f7e2484d93ab66efb98306ee52bdfdf0bfe
Jason Capriotti <jason.capriotti@gmail.com>
2014-10-14 15:16:34 -0500

    ConsoleZ.WithPin

---
+++ ConsoleZ.WithPin/README.md
@@ -0,0 +1,6 @@
+ConsoleZ.WithPin
+=================
+
+I like to pin things to my taskbar. This package installs a specific version of ConsoleZ and pins it to the taskbar.
+
+Because ConsoleZ installs via Chocolatey to a versioned package folder, my pinning package is tied to a specific version\package of ConsoleZ.
---
+++ ConsoleZ.WithPin/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$packageName = "ConsoleZ.WithPin"
+
+try 
+{
+	$myToolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+	$lib = (Get-Item $myToolsPath).Parent.Parent.FullName
+	$consoleZPath = Join-Path $lib "\ConsoleZ.1.12.0.14282\tools\console.exe"
+
+	Install-ChocolateyPinnedTaskBarItem $consoleZPath
+	
+	Write-ChocolateySuccess "$packageName"
+} 
+catch 
+{
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+	throw
+}

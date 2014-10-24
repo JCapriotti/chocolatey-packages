6439398423b3cb2fdea9aa604a0bce44b077892b
Jason Capriotti <jason.capriotti@gmail.com>
2014-10-24 14:31:29 -0500

    NuGet context menu

---
+++ NuGet.ContextMenu/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿
+$packageName = "NuGet.ContextMenu"
+
+try 
+{
+	New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+	$key = "HKCR:\nuspec_auto_file\shell\NuGet Pack\command"
+	New-Item "HKCR:\nuspec_auto_file\shell" -Name "NuGet Pack" -Force
+	New-Item "HKCR:\nuspec_auto_file\shell\NuGet Pack" -Name "command"  -Force
+	Set-Item $key -Value "cmd /c nuget pack ""%1""" -Force
+	Write-ChocolateySuccess "$packageName"
+} 
+catch 
+{
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+	throw
+}

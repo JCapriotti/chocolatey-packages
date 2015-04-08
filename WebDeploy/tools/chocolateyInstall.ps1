edbbbbc501656dc094a0c1fd23d82d6cf82eeb4d
Jason Capriotti <jason.capriotti@gmail.com>
2015-04-08 13:50:55 -0500

    Setting install options to all
    In current Chocolatey.org package but must have forgot to commit this :/

--- WebDeploy/tools/chocolateyInstall.ps1
+++ WebDeploy/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = 'Web Deploy 3.5'
 $installerType = 'MSI'
 $url = 'http://download.microsoft.com/download/D/4/4/D446D154-2232-49A1-9D64-F5A9429913A4/WebDeploy_x86_en-US.msi'
 $url64 = 'http://download.microsoft.com/download/D/4/4/D446D154-2232-49A1-9D64-F5A9429913A4/WebDeploy_amd64_en-US.msi'
-$silentArgs = '/qn /passive /norestart'
+$silentArgs = '/qn /passive /norestart ADDLOCAL=ALL'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
